79814e716384c0fdb1d578fc2c61402b341a5c82
Alexey Grigoriev <alexeygrigoriev167@gmail.com>
2026-05-07 15:28:09 +0300

    remove bootsrap js import. Not used.

--- src/app/Views/layout.php
+++ src/app/Views/layout.php
@@ -35,10 +35,6 @@
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
             integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
             crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ff"
-            crossorigin="anonymous"
-    ></script>
     <script src="<?= base_url("js/comments.js") ?>"></script>
 </body>
 </html>
